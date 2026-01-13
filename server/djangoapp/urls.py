82370ffe92c0a33b1256b109d217c3c35b5a8078
steckmera <63511994+steckmera@users.noreply.github.com>
2026-01-13 11:55:00 -0500

    Fix duplicate name for add_review path

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews,
          name='dealer_details'),
     path(route='add_review', view=views.add_review,
-         name='add_review'),    
+         name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
