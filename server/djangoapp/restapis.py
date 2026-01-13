ddaf60ae16edc88317c070c7fdfb1a6494fe6c43
steckmera <63511994+steckmera@users.noreply.github.com>
2026-01-13 11:40:45 -0500

    Fix parameter formatting in get_request function

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,13 +11,14 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 def get_request(endpoint, **kwargs):
     params = ""
     if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
 
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
